4a09b64c23e2b2c48bcc8f44284f3004baab6737
= <=>
2024-12-19 09:47:35 -0300

    adicionando sqlmodel template

---
+++ codigosaula/19122024/database.py
@@ -0,0 +1,20 @@
+from sqlmodel import create_engine, Session, SQLModel
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv("db.env")
+
+
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL,echo=True)
+
+def criar_db_and_tables():
+	SQLModel.metadata.create_all(engine)
+
+
+
+def get_session() -> Session:
+	return Session(engine)
